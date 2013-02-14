dd828f6c5e09b386e9b31f9990f8b8e48335af38
Claus Seiberth <claus@Clauss-MacBook-Pro.local>
2013-02-14 19:35:41 +0100

    With Google Maps

--- www/js/basic.js
+++ www/js/basic.js
@@ -3,6 +3,11 @@
 
 $(document).ready(function() {
  
+	    //$('a[data-theme="app-ios"]').find('.ui-icon').remove();
+	    $('a[data-theme="app-ios"]').append('<div class="ios-tip"><span>&nbsp;</span></div>');
+
+		
+
     var InfiniteRotator =
     {
         init: function()
@@ -47,11 +52,97 @@ $(function () {
     }());
 });
 
+/*google.maps.event.addListenerOnce(map, 'idle', function() {
+    google.maps.event.trigger(map, 'resize');
+    map.setCenter(point); // be sure to reset the map center as well
+});*/
 
-
+//$( '#page_5_1' ).live( 'pagebeforeshow',function(event, ui){
+$( '#page_5_1' ).live( 'pageinit',function(event, ui){
 	
+//alert('Hallo');
+	//$("#map_div").googlemap().getMap().checkResize();				
+	
+	//google.maps.event.trigger($('#map_div').gmap3("get"), "resize");
+	//map.checkResize(); 
+				//$('#map_div').trigger('create');
+				// tell google to resize the map
+				//google.maps.event.trigger(map, 'resize');
+				//resizeMap();
+				
+				//google.maps.event.trigger(map, 'resize');
+			    //map.setCenter(point); // be sure to reset the map center as well
+});
+
+//pagecreate
+//pageinit
+//pageshow
+$( '#page_5_1' ).live( 'pageshow',function(event, ui){
+
+	//------- Google Maps ---------//
+
+	// Creating a LatLng object containing the coordinate for the center of the map
+	var latlng = new google.maps.LatLng(50.7686653,6.1086211);
+
+	// Creating an object literal containing the properties we want to pass to the map  
+	var options = {  
+		zoom: 15, // This number can be set to define the initial zoom level of the map
+		center: latlng,
+		panControl: false,
+		zoomControl: true,
+		mapTypeControl: false,
+		scaleControl: false,
+		streetViewControl: false,
+		overviewMapControl: false,
+		mapTypeId: google.maps.MapTypeId.ROADMAP // This value can be set to define the map type ROADMAP/SATELLITE/HYBRID/TERRAIN
+	};  
+	
+	// Calling the constructor, thereby initializing the map  
+	var map = new google.maps.Map(document.getElementById('map_div'), options);  
+		//$('#map_div').trigger('create');
+		
+
+		
+	// Define Marker properties
+	var image = new google.maps.MarkerImage('images/marker.png',
+		// This marker is 129 pixels wide by 42 pixels tall.
+		new google.maps.Size(129, 42),
+		// The origin for this image is 0,0.
+		new google.maps.Point(0,0),
+		// The anchor for this image is the base of the flagpole at 18,42.
+		new google.maps.Point(18, 42)
+	);
+
+	// Add Marker
+	var marker1 = new google.maps.Marker({
+		position: new google.maps.LatLng(50.7686653,6.1086211), 
+		map: map,		
+		//icon: image // This path is the custom pin to be shown. Remove this line and the proceeding comma to use default pin
+	});	
+
+	// Add listener for a click on the pin
+	google.maps.event.addListener(marker1, 'click', function() {  
+		infowindow1.open(map, marker1);  
+	});
+
+	// Add information window
+	var infowindow1 = new google.maps.InfoWindow({  
+		content:  createInfo('ANWALTSKANZLEI BEX', 'Rechtsanwalt Harald Bex<br>Viktoriastraße 28<br>52066 Aachen')
+	}); 
+
+	// Create information window
+	function createInfo(title, content) {
+		return '<div class="infowindow"><strong>'+ title +'</strong><br />'+content+'</div>';
+	}
+	
+	//$('#map_div').trigger('create');
+	//------- Google Maps ---------//
+	//google.maps.event.trigger(map,'resize');
+	
+	//setTimeout(function(){map.invalidateSize();}, 1);
 
 
+});
 
 
 
